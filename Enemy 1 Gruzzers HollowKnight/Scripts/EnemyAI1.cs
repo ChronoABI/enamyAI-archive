a1de2807d510f628a9de365c9d103c6dad54b7a1
chronoabi <abishekpoudel2014@gmail.com>
2023-03-11 18:35:09 +0545

    script updated

--- Enemy 1 Gruzzers HollowKnight/Scripts/EnemyAI1.cs
+++ Enemy 1 Gruzzers HollowKnight/Scripts/EnemyAI1.cs
@@ -3,67 +3,73 @@ using UnityEngine;
 public class EnemyAI1 : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
-    [SerializeField] Vector2 moveDirection = new Vector2(1f,0.25f);
+    [SerializeField] Vector2 moveDirection = new Vector2(1f, 0.25f);
     [SerializeField] GameObject rightCheck, roofCheck, groundCheck;
-    [SerializeField] Vector2 rightCheckSize,roofCheckSize,groundCheckSize;
-    [SerializeField] LayerMask groundLayer, platform;
-    [SerializeField] bool goingUp = true;
-
-
+    [SerializeField] Vector2 rightCheckSize, roofCheckSize, groundCheckSize;
+    [SerializeField] LayerMask groundLayer, platform  ;
+    [SerializeField]bool goingUp = true;
 
     private bool touchedGround, touchedRoof, touchedRight;
     private Rigidbody2D EnemyRB;
 
-    void Start() 
+
+    void Start()
     {
         EnemyRB = GetComponent<Rigidbody2D>();
     }
 
-    void Update() 
+    void Update()
     {
-        EnemyRB.velocity = moveDirection * moveSpeed * Time.deltaTime;
-
         HitLogic();
     }
 
-   
+    void FixedUpdate()
+    {
+        EnemyRB.velocity = moveDirection * moveSpeed;
+    }
 
-    void HitLogic(){
-        touchedGround = HitDetector(groundCheck,groundCheckSize,(groundLayer | platform ));
-        touchedRoof = HitDetector(roofCheck,roofCheckSize,(groundLayer | platform));
-        touchedRight = HitDetector(rightCheck,rightCheckSize,(groundLayer | platform));
-        
-        if(touchedRight){
+    void HitLogic()
+    {
+        touchedRight = HitDetector(rightCheck, rightCheckSize, (groundLayer | platform));
+        touchedRoof = HitDetector(roofCheck, roofCheckSize, (groundLayer | platform));
+        touchedGround = HitDetector(groundCheck, groundCheckSize, (groundLayer | platform ));
+
+        if (touchedRight)
+        {
             Flip();
         }
-        if(touchedRoof && goingUp){
+        if (touchedRoof && goingUp)
+        {
             ChangeYDirection();
         }
-        if(touchedGround && !goingUp){
+        if (touchedGround && !goingUp)
+        {
             ChangeYDirection();
         }
     }
 
- bool HitDetector(GameObject gameObject,Vector2 size, LayerMask layer ){
-       return Physics2D.OverlapBox(gameObject.transform.position, size,0f, layer);
+    bool HitDetector(GameObject gameObject, Vector2 size, LayerMask layer)
+    {
+        return Physics2D.OverlapBox(gameObject.transform.position, size, 0f, layer);
     }
 
-    void ChangeYDirection(){
+    void ChangeYDirection()
+    {
         moveDirection.y = -moveDirection.y;
-        goingUp=!goingUp;
+        goingUp = !goingUp;
     }
 
-    void Flip(){
-        transform.Rotate(new Vector2(0,180));
-        moveDirection.x= -moveDirection.x;
+    void Flip()
+    {
+        transform.Rotate(new Vector2(0, 180));
+        moveDirection.x = -moveDirection.x;
     }
 
-    private void OnDrawGizmosSelected() {
+    private void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(groundCheck.transform.position,groundCheckSize);
-        Gizmos.DrawWireCube(roofCheck.transform.position,roofCheckSize);
-        Gizmos.DrawWireCube(rightCheck.transform.position,rightCheckSize);
+        Gizmos.DrawWireCube(groundCheck.transform.position, groundCheckSize);
+        Gizmos.DrawWireCube(roofCheck.transform.position, roofCheckSize);
+        Gizmos.DrawWireCube(rightCheck.transform.position, rightCheckSize);
     }
-
-
 }
